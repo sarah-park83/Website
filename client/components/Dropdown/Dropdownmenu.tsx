961405c15f9a56c552a8d2ad4021e846a6f5f866
martin corpus <nasuntao@yahoo.com>
2023-08-26 19:42:43 +1200

    prep dropdown components

--- client/components/Dropdown/Dropdownmenu.tsx
+++ client/components/Dropdown/Dropdownmenu.tsx
@@ -1,6 +1,11 @@
-import '../styles/dropdownmenu.css'
+import '../../styles/dropdownmenu.css'
 import logo from '/images/logo.png'
 import Dropdown1 from './Dropdown-1-체육회 소개'
+import Dropdown2 from './Dropdown-2-단체소개'
+import Dropdown3 from './Dropdown-3-대회정보'
+import Dropdown4 from './Dropdown-4-알림마당'
+import Dropdown5 from './Dropdown-5-자료실'
+import Dropdown6 from './Dropdown-6-협력기관'
 // import { Link } from 'react-router-dom'
 
 export default function Dropdownmenu() {
@@ -11,12 +16,12 @@ export default function Dropdownmenu() {
       </a>
 
       <Dropdown1 />
+      <Dropdown2 />
+      <Dropdown3 />
+      <Dropdown4 />
+      <Dropdown5 />
+      <Dropdown6 />
 
-      <a href="#association-info">단체소개</a>
-      <a href="#info">대회정보</a>
-      <a href="#news">알림마당</a>
-      <a href="#docs">자료실</a>
-      <a href="#coorporates">협력기관</a>
       <a href="#news">회원가입/회원증 확인</a>
     </div>
   )

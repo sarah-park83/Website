2db7305eb31c15746cc7e292c06c6df99beb382b
martin corpus <nasuntao@yahoo.com>
2023-08-28 10:03:01 +1200

    create location container

--- client/components/Links/총재인사말.tsx
+++ client/components/Links/총재인사말.tsx
@@ -1,8 +1,10 @@
+import '../../styles/subpage.css'
 import Dropdownmenu from '../Dropdown/Dropdownmenu'
 import Footer from '../Footer'
 import Header from './Link-header'
-
-// import '../styles/homepage.css'
+import { MdHome } from 'react-icons/md'
+import { BiSolidRightArrow } from 'react-icons/bi'
+import { Link } from 'react-router-dom'
 
 export function 총재인사말() {
   return (
@@ -11,7 +13,35 @@ export function 총재인사말() {
 
       <Header />
 
-      <h1>...info/greet...</h1>
+      <div className="subpage-container">
+        <div className="location-container">
+          <MdHome
+            focusable="false"
+            aria-hidden="true"
+            viewBox="0 0 24 24"
+            className="homeIcon"
+          />
+          <Link to="/">
+            <h2>홈</h2>
+          </Link>
+          <BiSolidRightArrow
+            focusable="false"
+            aria-hidden="true"
+            viewBox="0 0 24 24"
+            className="icon-style"
+          />
+          <p>체육회소개</p>
+          <BiSolidRightArrow
+            focusable="false"
+            aria-hidden="true"
+            viewBox="0 0 24 24"
+            className="icon-style"
+          />
+          <Link to="/info/greet">
+            <h2>총재 인사말</h2>
+          </Link>
+        </div>
+      </div>
 
       <Footer />
     </div>

183a18223150e3717c06f5bdd09d4c7b1410a016
martin corpus <nasuntao@yahoo.com>
2023-08-27 18:05:06 +1200

    prep first link page

---
+++ client/components/links/총재인사말.tsx
@@ -0,0 +1,18 @@
+import Dropdownmenu from '../Dropdown/Dropdownmenu'
+import Footer from '../Footer'
+
+// import '../styles/homepage.css'
+
+export function 총재인사말() {
+  return (
+    <div>
+      <Dropdownmenu />
+
+      {/* links header */}
+
+      <h1>...info/greet...</h1>
+
+      <Footer />
+    </div>
+  )
+}
--- client/routes.tsx
+++ client/routes.tsx
@@ -7,10 +7,13 @@ import {
 import App from './components/App'
 import { Homepage } from './components/Homepage'
 
+import { 총재인사말 } from './components/links/총재인사말'
+
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index element={<Homepage />} />
+      <Route path="/info/greet" element={<총재인사말 />} />
     </Route>
   )
 )

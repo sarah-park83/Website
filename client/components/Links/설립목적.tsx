7fcc7f9610603978377bf0be9236f8a5c3ac901c
martin corpus <nasuntao@yahoo.com>
2023-08-30 11:03:20 +1200

    finish 설립목적 link

--- client/components/Links/설립목적.tsx
+++ client/components/Links/설립목적.tsx
@@ -88,11 +88,61 @@ export function 설립목적() {
             <div className="purpose-content-container">
               <h2>스포츠가 최고의 국민건강 복지다</h2>
               <ul className="purpose-list">
-                <li className="purpose-item-container">
+                <li className="purpose-item-container class1">
                   <div className="purpose-number">1</div>
                   <h3 className="purpose-content">
                     생활체육 내외의 스포츠 클럽 활동을 지원,육성하여 국민건강
                     복지증진에 기여
+                  </h3>
+                </li>
+                <li className="purpose-item-container class2">
+                  <div className="purpose-number">2</div>
+                  <h3 className="purpose-content">
+                    전국 각 생활체육단위 스포츠클럽 활동지원 육성
+                  </h3>
+                </li>
+                <li className="purpose-item-container class3">
+                  <div className="purpose-number">3</div>
+                  <h3 className="purpose-content">
+                    생활체육 스포츠 클럽 리그 및 각종 공식대회 개최
+                  </h3>
+                </li>
+                <li className="purpose-item-container class4">
+                  <div className="purpose-number">4</div>
+                  <h3 className="purpose-content">
+                    전국 생활인 및 체육동호인 1인 1기 스포츠 참여 운동 전개
+                  </h3>
+                </li>
+                <li className="purpose-item-container class5">
+                  <div className="purpose-number">5</div>
+                  <h3 className="purpose-content">
+                    생활체육 스포츠 전문지도자 및 심판 양성교육
+                  </h3>
+                </li>
+                <li className="purpose-item-container class6">
+                  <div className="purpose-number">6</div>
+                  <h3 className="purpose-content">
+                    생활 체육관련 시설 위탁운영 및 학원. 연수원운영, 체육지도자
+                    파견
+                  </h3>
+                </li>
+                <li className="purpose-item-container class7">
+                  <div className="purpose-number">7</div>
+                  <h3 className="purpose-content">
+                    회원의 권익 향상을 위한 회원카드 발급 및 복리증진
+                  </h3>
+                </li>
+                <li className="purpose-item-container class8">
+                  <div className="purpose-number">8</div>
+                  <h3 className="purpose-content">
+                    민간자격의 자격기본법에 의한 생활체육행정사 및 각
+                    종목별단체의 자격제도의 관리 운영에 관한 사항
+                  </h3>
+                </li>
+                <li className="purpose-item-container class9">
+                  <div className="purpose-number">9</div>
+                  <h3 className="purpose-content">
+                    생활체육 육성을 위한 국제적 영향력을 행사한다
                   </h3>
                 </li>
               </ul>

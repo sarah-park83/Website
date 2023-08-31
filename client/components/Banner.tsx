c3e20eb985c655d3e9a98fc3eaf4d2201d744959
Sarah Park <sarahypark83@gmail.com>
2023-09-01 02:27:30 +1200

    update banner animation and font

--- client/components/Banner.tsx
+++ client/components/Banner.tsx
@@ -2,38 +2,50 @@ import { useEffect, useState } from 'react'
 import '../styles/banner.css'
 
 export default function Banner() {
-  const [opacity, setOpacity] = useState(0)
+  const [currentTextIndex, setCurrentTextIndex] = useState(0)
+  const [animationsDone, setAnimationsDone] = useState(false)
+
+  const textArray = [
+    // eslint-disable-next-line react/jsx-key
+    <h3>국민의 건강과</h3>,
+    // eslint-disable-next-line react/jsx-key
+    <h3>행복의 장을 여는</h3>,
+    // eslint-disable-next-line react/jsx-key
+    <h1>뉴질랜드대한생활체육회</h1>,
+    // eslint-disable-next-line react/jsx-key
+    <h4>New Zealand Korea Sports For All Athletic Association</h4>,
+  ]
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setOpacity((prevOpacity) => {
-        if (prevOpacity >= 1) {
-          clearInterval(interval)
-          return 1
-        }
-        return prevOpacity + 0.1
-      })
-    }, 100)
+    if (currentTextIndex < textArray.length - 1) {
+      const timeout = setTimeout(() => {
+        setCurrentTextIndex((prevIndex) => prevIndex + 1)
+      }, 2000)
 
-    return () => clearInterval(interval)
-  }, [])
+      return () => clearTimeout(timeout)
+    } else {
+      setTimeout(() => {
+        setAnimationsDone(true)
+      }, 2000)
+    }
+  }, [currentTextIndex, textArray.length])
 
   return (
     <div className="homeHeader_banner">
       <div className="homeHeader_banner_content">
         <div className="homeHeader_banner_text">
-          <h3 className="banner_text" style={{ opacity }}>
-            국민의 건강과
-          </h3>
-          <h3 className="banner_text" style={{ opacity }}>
-            행복의 장을 여는
-          </h3>
-          <h1 className="banner_text" style={{ opacity }}>
-            뉴질랜드대한생활체육회
-          </h1>
-          <p className="banner_text" style={{ opacity }}>
-            New Zealand Korea Sports For All Athletic Association
-          </p>
+          {textArray.map((text, index) => (
+            <div
+              key={index}
+              className="banner_text"
+              style={{
+                opacity: index <= currentTextIndex || animationsDone ? 1 : 0,
+                transition: 'opacity 1s ease',
+              }}
+            >
+              {text}
+            </div>
+          ))}
         </div>
       </div>
     </div>

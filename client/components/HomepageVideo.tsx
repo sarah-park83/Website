d1b2842e74789f9024e92acad36e9c699a951a78
Sarah Park <sarahypark83@gmail.com>
2023-09-04 01:18:21 +1200

    add css to homepage video

--- client/components/HomepageVideo.tsx
+++ client/components/HomepageVideo.tsx
@@ -1,13 +1,30 @@
+import { CSSProperties } from 'react'
+import '../styles/homepage-video.css'
 /* eslint-disable jsx-a11y/iframe-has-title */
 export default function HomepageVideo() {
+  const videoVideoStyle = {
+    width: '640px',
+    height: '360px',
+  }
+  const iframeStyle = {
+    width: '100%',
+    height: '100%',
+  }
   return (
     <div className="video-container">
-      <iframe
-        width="889"
-        height="500"
-        src="https://www.youtube.com/embed/-Gh3OUIO2WA"
-        allowFullScreen
-      ></iframe>
+      <div className="video-video" style={videoVideoStyle}>
+        <div className="video-frame">
+          <iframe
+            width="100%"
+            height="100%"
+            src="https://www.youtube.com/embed/-Gh3OUIO2WA"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            title="대한생활체육회를 소개합니다"
+            style={iframeStyle}
+            className="video-iframe"
+          ></iframe>
+        </div>
+      </div>
     </div>
   )
 }

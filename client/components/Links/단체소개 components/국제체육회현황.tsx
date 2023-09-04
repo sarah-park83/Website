c2bb4167b6bd974bb17abc34c76d5e63d8c871b6
martin corpus <nasuntao@yahoo.com>
2023-09-05 10:44:16 +1200

    finish  국제체육회현황 link

--- client/components/Links/단체소개 components/국제체육회현황.tsx
+++ client/components/Links/단체소개 components/국제체육회현황.tsx
@@ -110,6 +110,260 @@ export function 국제체육회현황() {
                   </p>
                 </div>
               </div>
+
+              <div className="group-list-container">
+                <div className="group-list-title">한인호주대한생활체육회</div>
+                <div className="group-list-img-container">
+                  <div className="group-list-img-container2">
+                    <button
+                      onClick={(e) => {
+                        e.preventDefault()
+                        window.open(
+                          'https://firebasestorage.googleapis.com/v0/b/sportsassorenual.appspot.com/o/group%2Faus.png?alt=media&amp;token=571fe9b5-f3df-48d4-b6a6-b7b87520ca4a',
+                          '_blank',
+                          'width=380,height=500,top=100,left=100'
+                        )
+                      }}
+                    >
+                      <img
+                        className="group-image"
+                        src="https://firebasestorage.googleapis.com/v0/b/sportsassorenual.appspot.com/o/group%2Faus.png?alt=media&amp;token=571fe9b5-f3df-48d4-b6a6-b7b87520ca4a"
+                        alt="한인호주대한생활체육회"
+                      />
+                    </button>
+                  </div>
+                </div>
+                <div className="group-list-text-container">
+                  <p>
+                    회장 : 송성근
+                    <br />
+                    체육회명 : 한인호주대한생활체육회
+                  </p>
+                </div>
+              </div>
+
+              <div className="group-list-container">
+                <div className="group-list-title">한인일본대한생활체육회</div>
+                <div className="group-list-img-container">
+                  <div className="group-list-img-container2">
+                    <button
+                      onClick={(e) => {
+                        e.preventDefault()
+                        window.open(
+                          'https://firebasestorage.googleapis.com/v0/b/sportsassorenual.appspot.com/o/group%2Fjapan.png?alt=media&amp;token=e36b91d6-589c-4a73-a1b5-8a5a07682f39',
+                          '_blank',
+                          'width=380,height=500,top=100,left=100'
+                        )
+                      }}
+                    >
+                      <img
+                        className="group-image"
+                        src="https://firebasestorage.googleapis.com/v0/b/sportsassorenual.appspot.com/o/group%2Fjapan.png?alt=media&amp;token=e36b91d6-589c-4a73-a1b5-8a5a07682f39"
+                        alt="한인일본대한생활체육회"
+                      />
+                    </button>
+                  </div>
+                </div>
+                <div className="group-list-text-container">
+                  <p>
+                    회장 : 전진영
+                    <br />
+                    체육회명 : 한인일본대한생활체육회
+                  </p>
+                </div>
+              </div>
+
+              <div className="group-list-container">
+                <div className="group-list-title">한인중국대한생활체육회</div>
+                <div className="group-list-img-container">
+                  <div className="group-list-img-container2">
+                    <button
+                      onClick={(e) => {
+                        e.preventDefault()
+                        window.open(
+                          'https://firebasestorage.googleapis.com/v0/b/sportsassorenual.appspot.com/o/group%2Fchina.png?alt=media&amp;token=0bb1b78f-88f7-4539-bdc1-91eac469f37b',
+                          '_blank',
+                          'width=380,height=500,top=100,left=100'
+                        )
+                      }}
+                    >
+                      <img
+                        className="group-image"
+                        src="https://firebasestorage.googleapis.com/v0/b/sportsassorenual.appspot.com/o/group%2Fchina.png?alt=media&amp;token=0bb1b78f-88f7-4539-bdc1-91eac469f37b"
+                        alt="한인중국대한생활체육회"
+                      />
+                    </button>
+                  </div>
+                </div>
+                <div className="group-list-text-container">
+                  <p>
+                    회장 : 장응만
+                    <br />
+                    체육회명 : 한인중국대한생활체육회
+                  </p>
+                </div>
+              </div>
+
+              <div className="group-list-container">
+                <div className="group-list-title">한인태국대한생활체육회</div>
+                <div className="group-list-img-container">
+                  <div className="group-list-img-container2">
+                    <button
+                      onClick={(e) => {
+                        e.preventDefault()
+                        window.open(
+                          'https://firebasestorage.googleapis.com/v0/b/sportsassorenual.appspot.com/o/group%2Ftai.png?alt=media&amp;token=e2342dc9-d109-4448-b36e-e99c0ec64d5f',
+                          '_blank',
+                          'width=380,height=500,top=100,left=100'
+                        )
+                      }}
+                    >
+                      <img
+                        className="group-image"
+                        src="https://firebasestorage.googleapis.com/v0/b/sportsassorenual.appspot.com/o/group%2Ftai.png?alt=media&amp;token=e2342dc9-d109-4448-b36e-e99c0ec64d5f"
+                        alt="한인태국대한생활체육회"
+                      />
+                    </button>
+                  </div>
+                </div>
+                <div className="group-list-text-container">
+                  <p>
+                    회장 : 홍성호
+                    <br />
+                    체육회명 : 한인태국대한생활체육회
+                  </p>
+                </div>
+              </div>
+
+              <div className="group-list-container">
+                <div className="group-list-title">한인괌대한생활체육회</div>
+                <div className="group-list-img-container">
+                  <div className="group-list-img-container2">
+                    <button
+                      onClick={(e) => {
+                        e.preventDefault()
+                        window.open(
+                          'https://firebasestorage.googleapis.com/v0/b/sportsassorenual.appspot.com/o/group%2Fggg.png?alt=media&amp;token=8ae5344d-8af3-4b2a-8e69-770d2e00c16e',
+                          '_blank',
+                          'width=380,height=500,top=100,left=100'
+                        )
+                      }}
+                    >
+                      <img
+                        className="group-image"
+                        src="https://firebasestorage.googleapis.com/v0/b/sportsassorenual.appspot.com/o/group%2Fggg.png?alt=media&amp;token=8ae5344d-8af3-4b2a-8e69-770d2e00c16e"
+                        alt="한인괌대한생활체육회"
+                      />
+                    </button>
+                  </div>
+                </div>
+                <div className="group-list-text-container">
+                  <p>
+                    회장 : 조진영
+                    <br />
+                    체육회명 : 한인괌대한생활체육회
+                  </p>
+                </div>
+              </div>
+
+              <div className="group-list-container">
+                <div className="group-list-title">
+                  한인말레이시아대한생활체육회
+                </div>
+                <div className="group-list-img-container">
+                  <div className="group-list-img-container2">
+                    <button
+                      onClick={(e) => {
+                        e.preventDefault()
+                        window.open(
+                          'https://firebasestorage.googleapis.com/v0/b/sportsassorenual.appspot.com/o/group%2Fmal.png?alt=media&amp;token=ef564b75-3cf4-46b8-a7b7-a28f176ba8c6',
+                          '_blank',
+                          'width=380,height=500,top=100,left=100'
+                        )
+                      }}
+                    >
+                      <img
+                        className="group-image"
+                        src="https://firebasestorage.googleapis.com/v0/b/sportsassorenual.appspot.com/o/group%2Fmal.png?alt=media&amp;token=ef564b75-3cf4-46b8-a7b7-a28f176ba8c6"
+                        alt="한인말레이시아대한생활체육회"
+                      />
+                    </button>
+                  </div>
+                </div>
+                <div className="group-list-text-container">
+                  <p>
+                    회장 : 전정진
+                    <br />
+                    체육회명 : 한인말레이시아대한생활체육회
+                  </p>
+                </div>
+              </div>
+
+              <div className="group-list-container">
+                <div className="group-list-title">
+                  한인캄보디아대한생활체육회
+                </div>
+                <div className="group-list-img-container">
+                  <div className="group-list-img-container2">
+                    <button
+                      onClick={(e) => {
+                        e.preventDefault()
+                        window.open(
+                          'https://firebasestorage.googleapis.com/v0/b/sportsassorenual.appspot.com/o/group%2Fcam.png?alt=media&amp;token=d3e941c4-a5e4-4800-bb66-77957c000571',
+                          '_blank',
+                          'width=380,height=500,top=100,left=100'
+                        )
+                      }}
+                    >
+                      <img
+                        className="group-image"
+                        src="https://firebasestorage.googleapis.com/v0/b/sportsassorenual.appspot.com/o/group%2Fcam.png?alt=media&amp;token=d3e941c4-a5e4-4800-bb66-77957c000571"
+                        alt="한인캄보디아대한생활체육회"
+                      />
+                    </button>
+                  </div>
+                </div>
+                <div className="group-list-text-container">
+                  <p>
+                    회장 : 이희석
+                    <br />
+                    체육회명 : 한인캄보디아대한생활체육회
+                  </p>
+                </div>
+              </div>
+
+              <div className="group-list-container">
+                <div className="group-list-title">
+                  한인뉴질랜드대한생활체육회
+                </div>
+                <div className="group-list-img-container">
+                  <div className="group-list-img-container2">
+                    <button
+                      onClick={(e) => {
+                        e.preventDefault()
+                        window.open(
+                          'https://firebasestorage.googleapis.com/v0/b/sportsassorenual.appspot.com/o/group%2Fkwk.png?alt=media&amp;token=38cba1ea-7e20-4dba-8ec1-98d7ed8e1115',
+                          '_blank',
+                          'width=380,height=500,top=100,left=100'
+                        )
+                      }}
+                    >
+                      <img
+                        className="group-image"
+                        src="https://firebasestorage.googleapis.com/v0/b/sportsassorenual.appspot.com/o/group%2Fkwk.png?alt=media&amp;token=38cba1ea-7e20-4dba-8ec1-98d7ed8e1115"
+                        alt="한인뉴질랜드대한생활체육회"
+                      />
+                    </button>
+                  </div>
+                </div>
+                <div className="group-list-text-container">
+                  <p>
+                    회장 : 김웅기
+                    <br />
+                    체육회명 : 한인뉴질랜드대한생활체육회
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>

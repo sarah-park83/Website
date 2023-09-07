0f0df3f01f0160df9cd6fee824c19595f48eb0dd
martin corpus <nasuntao@yahoo.com>
2023-09-07 12:54:40 +1200

    update  responsivenesss for dropdown

--- client/components/Dropdown/Dropdownmenu-mobile.tsx
+++ client/components/Dropdown/Dropdownmenu-mobile.tsx
@@ -1,8 +1,41 @@
 import '../../styles/dropdownmenu-mobile.css'
 import logo from '/images/logo.png'
 import { FaBars } from 'react-icons/fa'
+import { BiSolidDownArrow, BiSolidUpArrow } from 'react-icons/bi'
+import { RxCross2 } from 'react-icons/rx'
+
+import { useEffect, useState, useCallback } from 'react'
 
 export default function Dropdownmenu_mobile() {
+  const [isMenuActive, setIsMenuActive] = useState(false)
+
+  const toggleMobileNavMenu = useCallback(() => {
+    const mobileNavMenu = document.querySelector('.mobile-nav-menu')
+    if (mobileNavMenu) {
+      mobileNavMenu.classList.toggle('active')
+      setIsMenuActive(!isMenuActive)
+    }
+  }, [isMenuActive])
+
+  useEffect(() => {
+    const menuIcon = document.getElementById('menu-icon')
+    if (menuIcon) {
+      menuIcon.addEventListener('click', toggleMobileNavMenu)
+    }
+    return () => {
+      if (menuIcon) {
+        menuIcon.removeEventListener('click', toggleMobileNavMenu)
+      }
+    }
+  }, [toggleMobileNavMenu])
+
+  ////////////////////////////////////////////////
+  const [isExpanded, setIsExpanded] = useState(false)
+
+  const toggleMobileNav = () => {
+    setIsExpanded(!isExpanded)
+  }
+
   return (
     <>
       <div className="navbar-mobile">
@@ -33,7 +66,51 @@ export default function Dropdownmenu_mobile() {
           >
             회원가입 및 회원증 확인
           </a>
-          <FaBars focusable="false" aria-hidden="true" className="menu-icon" />
+          {isMenuActive ? (
+            <RxCross2
+              focusable="false"
+              aria-hidden="true"
+              className="menu-icon"
+              id="menu-icon"
+            />
+          ) : (
+            <FaBars
+              focusable="false"
+              aria-hidden="true"
+              className="menu-icon"
+              id="menu-icon"
+            />
+          )}
+        </div>
+        <div className="mobile-nav-menu">
+          <button
+            className={`mobile-nav-title ${
+              isExpanded ? 'mobile-nav-title-selected' : ''
+            }`}
+            onClick={toggleMobileNav}
+          >
+            체육회소개
+            {isExpanded ? (
+              <BiSolidUpArrow
+                focusable="false"
+                aria-hidden="true"
+                viewBox="0 0 24 24"
+                className="up-arrow-icon"
+              />
+            ) : (
+              <BiSolidDownArrow
+                focusable="false"
+                aria-hidden="true"
+                viewBox="0 0 24 24"
+                className="down-arrow-icon"
+              />
+            )}
+          </button>
+          {isExpanded && (
+            <a href="/info/greet">
+              <li className="mobile-nav-item">총재 인사말</li>
+            </a>
+          )}
         </div>
       </div>
     </>

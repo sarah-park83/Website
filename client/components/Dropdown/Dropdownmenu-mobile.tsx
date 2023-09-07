9ac29df13a941980e85508cd21a6b32754a5803d
martin corpus <nasuntao@yahoo.com>
2023-09-07 15:02:15 +1200

    finish mobile  responsivenesss for dropdown

--- client/components/Dropdown/Dropdownmenu-mobile.tsx
+++ client/components/Dropdown/Dropdownmenu-mobile.tsx
@@ -3,11 +3,17 @@ import logo from '/images/logo.png'
 import { FaBars } from 'react-icons/fa'
 import { BiSolidDownArrow, BiSolidUpArrow } from 'react-icons/bi'
 import { RxCross2 } from 'react-icons/rx'
-
+import { Link } from 'react-router-dom'
 import { useEffect, useState, useCallback } from 'react'
 
 export default function Dropdownmenu_mobile() {
   const [isMenuActive, setIsMenuActive] = useState(false)
+  const [isExpanded1, setIsExpanded1] = useState(false)
+  const [isExpanded2, setIsExpanded2] = useState(false)
+  const [isExpanded3, setIsExpanded3] = useState(false)
+  const [isExpanded4, setIsExpanded4] = useState(false)
+  const [isExpanded5, setIsExpanded5] = useState(false)
+  const [isExpanded6, setIsExpanded6] = useState(false)
 
   const toggleMobileNavMenu = useCallback(() => {
     const mobileNavMenu = document.querySelector('.mobile-nav-menu')
@@ -29,11 +35,58 @@ export default function Dropdownmenu_mobile() {
     }
   }, [toggleMobileNavMenu])
 
-  ////////////////////////////////////////////////
-  const [isExpanded, setIsExpanded] = useState(false)
+  const toggleMobileNav1 = () => {
+    setIsExpanded1(!isExpanded1)
+    setIsExpanded2(false)
+    setIsExpanded3(false)
+    setIsExpanded4(false)
+    setIsExpanded5(false)
+    setIsExpanded6(false)
+  }
 
-  const toggleMobileNav = () => {
-    setIsExpanded(!isExpanded)
+  const toggleMobileNav2 = () => {
+    setIsExpanded2(!isExpanded2)
+    setIsExpanded1(false)
+    setIsExpanded3(false)
+    setIsExpanded4(false)
+    setIsExpanded5(false)
+    setIsExpanded6(false)
+  }
+
+  const toggleMobileNav3 = () => {
+    setIsExpanded3(!isExpanded3)
+    setIsExpanded1(false)
+    setIsExpanded2(false)
+    setIsExpanded4(false)
+    setIsExpanded5(false)
+    setIsExpanded6(false)
+  }
+
+  const toggleMobileNav4 = () => {
+    setIsExpanded4(!isExpanded4)
+    setIsExpanded1(false)
+    setIsExpanded2(false)
+    setIsExpanded3(false)
+    setIsExpanded5(false)
+    setIsExpanded6(false)
+  }
+
+  const toggleMobileNav5 = () => {
+    setIsExpanded5(!isExpanded5)
+    setIsExpanded1(false)
+    setIsExpanded2(false)
+    setIsExpanded3(false)
+    setIsExpanded4(false)
+    setIsExpanded6(false)
+  }
+
+  const toggleMobileNav6 = () => {
+    setIsExpanded6(!isExpanded6)
+    setIsExpanded1(false)
+    setIsExpanded2(false)
+    setIsExpanded3(false)
+    setIsExpanded4(false)
+    setIsExpanded5(false)
   }
 
   return (
@@ -85,12 +138,12 @@ export default function Dropdownmenu_mobile() {
         <div className="mobile-nav-menu">
           <button
             className={`mobile-nav-title ${
-              isExpanded ? 'mobile-nav-title-selected' : ''
+              isExpanded1 ? 'mobile-nav-title-selected' : ''
             }`}
-            onClick={toggleMobileNav}
+            onClick={toggleMobileNav1}
           >
             체육회소개
-            {isExpanded ? (
+            {isExpanded1 ? (
               <BiSolidUpArrow
                 focusable="false"
                 aria-hidden="true"
@@ -106,10 +159,203 @@ export default function Dropdownmenu_mobile() {
               />
             )}
           </button>
-          {isExpanded && (
-            <a href="/info/greet">
-              <li className="mobile-nav-item">총재 인사말</li>
-            </a>
+          {isExpanded1 && (
+            <>
+              <Link to="/info/greet">
+                <div className="mobile-nav-item">총재 인사말</div>
+              </Link>
+              <Link to="/info/purpose">
+                <div className="mobile-nav-item">설립목적</div>
+              </Link>
+              <Link to="/info/chart">
+                <div className="mobile-nav-item">중앙 조직도</div>
+              </Link>
+              <Link to="/info/status">
+                <div className="mobile-nav-item">임원현황</div>
+              </Link>
+              <Link to="/info/location">
+                <div className="mobile-nav-item">오시는 길</div>
+              </Link>
+            </>
+          )}
+
+          <button
+            className={`mobile-nav-title ${
+              isExpanded2 ? 'mobile-nav-title-selected' : ''
+            }`}
+            onClick={toggleMobileNav2}
+          >
+            단체소개
+            {isExpanded2 ? (
+              <BiSolidUpArrow
+                focusable="false"
+                aria-hidden="true"
+                viewBox="0 0 24 24"
+                className="up-arrow-icon"
+              />
+            ) : (
+              <BiSolidDownArrow
+                focusable="false"
+                aria-hidden="true"
+                viewBox="0 0 24 24"
+                className="down-arrow-icon"
+              />
+            )}
+          </button>
+          {isExpanded2 && (
+            <>
+              <Link to="/group/nation">
+                <div className="mobile-nav-item">전국체육회현황</div>
+              </Link>
+              <Link to="/group/internation">
+                <div className="mobile-nav-item">국제체육회현황</div>
+              </Link>
+              <Link to="/group/sports">
+                <div className="mobile-nav-item">종목별운영현황</div>
+              </Link>
+              <Link to="/group/sanha">
+                <div className="mobile-nav-item">산하단체</div>
+              </Link>
+            </>
+          )}
+
+          <button
+            className={`mobile-nav-title ${
+              isExpanded3 ? 'mobile-nav-title-selected' : ''
+            }`}
+            onClick={toggleMobileNav3}
+          >
+            대회정보
+            {isExpanded3 ? (
+              <BiSolidUpArrow
+                focusable="false"
+                aria-hidden="true"
+                viewBox="0 0 24 24"
+                className="up-arrow-icon"
+              />
+            ) : (
+              <BiSolidDownArrow
+                focusable="false"
+                aria-hidden="true"
+                viewBox="0 0 24 24"
+                className="down-arrow-icon"
+              />
+            )}
+          </button>
+          {isExpanded3 && (
+            <>
+              <Link to="/notice/apply/1">
+                <div className="mobile-nav-item">대회신청</div>
+              </Link>
+              <Link to="/notice/schedule/1">
+                <div className="mobile-nav-item">대회/행사일정</div>
+              </Link>
+              <Link to="/notice/result/1">
+                <div className="mobile-nav-item">경기결과</div>
+              </Link>
+            </>
+          )}
+
+          <button
+            className={`mobile-nav-title ${
+              isExpanded4 ? 'mobile-nav-title-selected' : ''
+            }`}
+            onClick={toggleMobileNav4}
+          >
+            알림마당
+            {isExpanded4 ? (
+              <BiSolidUpArrow
+                focusable="false"
+                aria-hidden="true"
+                viewBox="0 0 24 24"
+                className="up-arrow-icon"
+              />
+            ) : (
+              <BiSolidDownArrow
+                focusable="false"
+                aria-hidden="true"
+                viewBox="0 0 24 24"
+                className="down-arrow-icon"
+              />
+            )}
+          </button>
+          {isExpanded4 && (
+            <>
+              <Link to="/notice/announcement/1">
+                <div className="mobile-nav-item">공지/소식사항</div>
+              </Link>
+              <Link to="/notice/media/1">
+                <div className="mobile-nav-item">언론보도</div>
+              </Link>
+              <Link to="/notice/photo/1">
+                <div className="mobile-nav-item">포토갤러리</div>
+              </Link>
+              <Link to="/notice/video/1">
+                <div className="mobile-nav-item">동영상갤러리</div>
+              </Link>
+            </>
+          )}
+
+          <button
+            className={`mobile-nav-title ${
+              isExpanded5 ? 'mobile-nav-title-selected' : ''
+            }`}
+            onClick={toggleMobileNav5}
+          >
+            자료실
+            {isExpanded5 ? (
+              <BiSolidUpArrow
+                focusable="false"
+                aria-hidden="true"
+                viewBox="0 0 24 24"
+                className="up-arrow-icon"
+              />
+            ) : (
+              <BiSolidDownArrow
+                focusable="false"
+                aria-hidden="true"
+                viewBox="0 0 24 24"
+                className="down-arrow-icon"
+              />
+            )}
+          </button>
+          {isExpanded5 && (
+            <>
+              <Link to="/notice/reference/1">
+                <div className="mobile-nav-item">자료실</div>
+              </Link>
+            </>
+          )}
+
+          <button
+            className={`mobile-nav-title ${
+              isExpanded6 ? 'mobile-nav-title-selected' : ''
+            }`}
+            onClick={toggleMobileNav6}
+          >
+            협력기관
+            {isExpanded6 ? (
+              <BiSolidUpArrow
+                focusable="false"
+                aria-hidden="true"
+                viewBox="0 0 24 24"
+                className="up-arrow-icon"
+              />
+            ) : (
+              <BiSolidDownArrow
+                focusable="false"
+                aria-hidden="true"
+                viewBox="0 0 24 24"
+                className="down-arrow-icon"
+              />
+            )}
+          </button>
+          {isExpanded6 && (
+            <>
+              <Link to="/mou/mou">
+                <div className="mobile-nav-item">협력기관</div>
+              </Link>
+            </>
           )}
         </div>
       </div>

f999d29bcc1d3be2ce04cfb9a13b8b06f8dedc9e
Sarah Park <sarahypark83@gmail.com>
2023-09-11 05:05:47 +1200

    update noticeboard and popup responsiveness

--- client/components/Noticeboard.tsx
+++ client/components/Noticeboard.tsx
@@ -13,13 +13,59 @@ export default function Noticeboard() {
             <li>+</li>
           </a>
         </ul>
-        <ul className="noticeboard_content_container">
+        <ul className="article_content_container">
           <a href="article/announcement/2">
-            <li>
+            <li className="article_list">
               <p>●</p>
               <h4 className="article_createdAt">2023.08.03</h4>
               <h4 className="article_title">
                 대한생활체육회 가입 회원 제휴 혜택 안내
+              </h4>
+            </li>
+          </a>
+          <a href="/article/anouncement/1/fXgAI1gW5oynoQoYvv7j">
+            <li>
+              <p>●</p>
+              <h4 className="article_createdAt">2022.02.10</h4>
+              <h4 className="article_title">
+                코로나 확산으로 인한 103주년 삼일절 특별행사 취소건
+              </h4>
+            </li>
+          </a>
+          <a href="/article/anouncement/1/Fq4EEqvfNaQEu98NiLDT">
+            <li>
+              <p>●</p>
+              <h4 className="article_createdAt">2022.02.06</h4>
+              <h4 className="article_title">
+                대한생활체육회 제103주년 삼일절 특별행사
+              </h4>
+            </li>
+          </a>
+          <a href="/article/anouncement/1/N61bnWsOzPx9VwEtyI1o">
+            <li>
+              <p>●</p>
+              <h4 className="article_createdAt">2021.12.24</h4>
+              <h4 className="article_title">
+                대한생활체육회(가) 고려대학교구로병원(과) 건강검진협약체결
+              </h4>
+            </li>
+          </a>
+          <a href="/article/anouncement/1/D8OubT5WRHgoXUirfBUl">
+            <li>
+              <p>●</p>
+              <h4 className="article_createdAt">2021.12.20</h4>
+              <h4 className="article_title">
+                대한생활체육회 2022년도 세부일정안내(2021.12.20)
+              </h4>
+            </li>
+          </a>
+          <a href="/article/anouncement/1/7tJjzsD9ZME7dgt04KrQ">
+            <li>
+              <p>●</p>
+              <h4 className="article_createdAt">2021.12.20</h4>
+              <h4 className="article_title">
+                대한생활체육회 (주)HJ 매그놀리아 용평 호텔 앤 리조트 MOU 제휴
+                안내
               </h4>
             </li>
           </a>

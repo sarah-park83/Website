7015bb46bd8339c65675ce6e5c8e9d0493c85bea
martin corpus <nasuntao@yahoo.com>
2023-09-12 13:41:25 +1200

    finish 대회행사일정Link16

--- client/routes.tsx
+++ client/routes.tsx
@@ -54,6 +54,10 @@ export const router = createBrowserRouter(
         element={<대회정보components.대회행사일정Link17 />}
       />
       <Route
+        path="/article/schedule/1/j47ok5LBuWdyXvn0Gx40"
+        element={<대회정보components.대회행사일정Link16 />}
+      />
+      <Route
         path="/notice/schedule/2"
         element={<대회정보components.대회행사일정p2 />}
       />

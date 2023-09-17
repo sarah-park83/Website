624e7e6a48c117cb2e3045f78062f58ae167e889
Sarah Park <sarahypark83@gmail.com>
2023-09-18 02:13:11 +1200

    make a gallery on homepage

---
+++ client/components/Gallery.tsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+
+import {
+  IoMdArrowDropleftCircle,
+  IoMdArrowDroprightCircle,
+} from 'react-icons/io'
+
+import '../styles/gallery.css'
+
+interface GalleryProps {
+  imagePaths: string[] // Array of image paths in your public directory
+}
+
+const Gallery: React.FC<GalleryProps> = ({ imagePaths }) => {
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  const prevImage = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex > 0 ? prevIndex - 1 : imagePaths.length - 1
+    )
+  }
+
+  const nextImage = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex < imagePaths.length - 1 ? prevIndex + 1 : 0
+    )
+  }
+
+  return (
+    <div className="gallery_container">
+      <div className="galleryimg_container">
+        <img
+          className="galleryimg"
+          src={imagePaths[currentIndex]}
+          alt={`${currentIndex + 1}`}
+        />
+        <div className="gallery_buttonContainer">
+          <button className="gallery_button" onClick={prevImage}>
+            <IoMdArrowDropleftCircle />
+          </button>
+          <button className="gallery_button" onClick={nextImage}>
+            <IoMdArrowDroprightCircle />
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Gallery
--- client/components/Homepage.tsx
+++ client/components/Homepage.tsx
@@ -1,6 +1,5 @@
 import Dropdownmenu from './Dropdown/Dropdownmenu'
 import Dropdownmenu_mobile from './Dropdown/Dropdownmenu-mobile'
-import Photogallery from './Photogallery'
 import Footer from './Footer'
 import Banner from './Banner'
 import HomepageVideo from './HomepageVideo'
@@ -8,23 +7,34 @@ import Popup from './Popup'
 import Intro from './Intro'
 import MobileCheck from './Dropdown/Mobile-check'
 import Tafisa from './Tafisa'
+import Gallery from './Gallery'
+
 import '../styles/homepage.css'
 
 export function Homepage() {
   const isMobile = MobileCheck()
-  console.log('isMobile:', isMobile)
+  const popupImagePaths = [
+    '/images/Gallery/image1.jpeg',
+    '/images/Gallery/image2.jpeg',
+    '/images/Gallery/image3.jpeg',
+  ]
+  const imagePaths = [
+    '/images/Gallery/image1.jpeg',
+    '/images/Gallery/image2.jpeg',
+    '/images/Gallery/image3.jpeg',
+  ]
   return (
     <div className="page-container">
       <div>{isMobile ? <Dropdownmenu_mobile /> : <Dropdownmenu />}</div>
       <Banner />
-      <Popup />
+      <Popup popupImagePaths={popupImagePaths} />
       <Tafisa />
       <div className="video-section">
         <HomepageVideo />
       </div>
       <div>
         <Intro />
-        <Photogallery />
+        <Gallery imagePaths={imagePaths} />
         <Footer />
       </div>
     </div>

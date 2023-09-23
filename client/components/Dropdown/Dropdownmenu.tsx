a52ae5084011cb911307aa63700bfbb9219263c8
Sarah Park <sarahypark83@gmail.com>
2023-09-24 10:35:06 +1300

    try auth0

--- client/components/Dropdown/Dropdownmenu.tsx
+++ client/components/Dropdown/Dropdownmenu.tsx
@@ -5,7 +5,31 @@ import Dropdown2 from './Dropdown-2-단체소개'
 import Dropdown4 from './Dropdown-4-알림마당'
 import Dropdown6 from './Dropdown-6-스폰서'
 
+import { useAuth0 } from '@auth0/auth0-react'
+import { IfNotAuthenticated, IfAuthenticated } from '../Authentication'
+import { useEffect, useState } from 'react'
+
 export default function Dropdownmenu() {
+  const { loginWithRedirect, logout, isAuthenticated } = useAuth0()
+
+  const handleSignIn = () => {
+    loginWithRedirect()
+  }
+
+  const handleSignOut = () => {
+    logout()
+    localStorage.removeItem('isAuthenticated') // Remove the flag on logout
+  }
+
+  useEffect(() => {
+    const savedAuthStatus = localStorage.getItem('isAuthenticated')
+    if (savedAuthStatus === 'true') {
+      isAuthenticated || localStorage.setItem('isAuthenticated', 'true')
+    } else {
+      isAuthenticated && localStorage.setItem('isAuthenticated', 'false')
+    }
+  }, [isAuthenticated])
+
   return (
     <div className="navbar">
       <a href="/">
@@ -17,7 +41,17 @@ export default function Dropdownmenu() {
       <Dropdown4 />
       <Dropdown6 />
 
-      <a href="#news">회원가입/회원증 확인</a>
+      {isAuthenticated ? (
+        <button onClick={handleSignOut}>로그아웃</button>
+      ) : (
+        <button onClick={handleSignIn} className="loginButton">
+          회원가입/회원증 확인
+        </button>
+      )}
+      {/* 
+      <IfAuthenticated>
+        <button onClick={handleSignOut}>로그아웃</button>
+      </IfAuthenticated> */}
     </div>
   )
 }
--- client/index.tsx
+++ client/index.tsx
@@ -3,14 +3,31 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
+import { Auth0Provider } from '@auth0/auth0-react'
 
 const queryClient = new QueryClient()
 
+declare module '@auth0/auth0-react' {
+  interface Auth0ProviderProps {
+    domain: string
+    clientId: string
+    redirectUri: string
+    audience: string
+  }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   createRoot(document.getElementById('app') as HTMLElement).render(
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    <Auth0Provider
+      domain="dev-ulco4wbyjptvmo24.au.auth0.com"
+      clientId="XNBSyJNL8Uhjg6dfwMOmHfD6lqErtPFl"
+      redirectUri={window.location.origin}
+      audience="https://www.nzksfaa.co.nz"
+    >
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </Auth0Provider>
   )
 })

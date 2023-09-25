dcc448606144a234f482884369c91b6f72791728
Sarah Park <sarahypark83@gmail.com>
2023-09-26 00:23:43 +1300

    update index.ts

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,11 @@ import server from './server'
 
 const PORT = process.env.PORT || 3000
 
-server.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log('Listening on port', PORT)
-})
+try {
+  server.listen(PORT, () => {
+    // eslint-disable-next-line no-console
+    console.log('Listening on port', PORT)
+  })
+} catch (error) {
+  console.error('Error starting the server:', error)
+}

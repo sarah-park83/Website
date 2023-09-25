347bafb81f3ca66515ddc1da63855b50f49b2643
Sarah Park <sarahypark83@gmail.com>
2023-09-25 23:43:45 +1300

    update server.tx

--- server/server.ts
+++ server/server.ts
@@ -3,24 +3,34 @@ import { join, resolve } from 'node:path'
 
 const server = express()
 
+// Serve TypeScript files as JavaScript modules
 server.get('*.ts', (req, res, next) => {
   res.type('application/javascript')
   next()
 })
 
-server.use(express.json())
-server.use(express.static(join(__dirname, 'public')))
+// Serve JavaScript files as JavaScript modules
 server.use('*.js', (req, res, next) => {
   res.type('application/javascript')
   next()
 })
 
+// Serve static files from the 'public' directory
+server.use(express.static(join(__dirname, 'public')))
+
 if (process.env.NODE_ENV === 'production') {
+  // Serve assets and images in production
   server.use('/assets', express.static(resolve(__dirname, '../assets')))
   server.use('/images', express.static(resolve(__dirname, './public/images')))
+
+  // Handle all other routes by serving 'index.html'
   server.get('*', (req, res) => {
     res.sendFile(resolve(__dirname, '../index.html'))
   })
 }
 
-export default server
+// Start the server
+const PORT = process.env.PORT || 3000
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})

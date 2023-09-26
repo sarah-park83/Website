6a1a3df1cc752ea5ff1a80ef71ba19ae7f8f1273
Sarah Park <sarahypark83@gmail.com>
2023-09-26 13:44:47 +1300

    update vite.config file, delete base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/<Website>/' : '/',
   server: {
     proxy: {
       '/api': 'http://localhost:3000',

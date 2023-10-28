ad505d06a57cb9cf29084fc37f9328b82aa312b7
Sarah Park <sarahypark83@gmail.com>
2023-10-28 15:42:03 +1300

    update gallery on homepage

--- client/components/Gallery.tsx
+++ client/components/Gallery.tsx
@@ -34,13 +34,13 @@ const Gallery: React.FC<GalleryProps> = ({ images }) => {
       <p className="gallery_title">뉴질랜드 대한생활체육회 활동</p>
       <div className="gallery_container">
         <div className="galleryimg_container">
-          {/* <a href={images[currentIndex].link}>
+          <a>
             <img
               className="galleryimg"
               src={images[currentIndex].path}
               alt={`${currentIndex + 1}`}
             />
-          </a> */}
+          </a>
           <div className="gallery_buttonContainer">
             <button className="gallery_button" onClick={prevImage}>
               <IoMdArrowDropleftCircle />

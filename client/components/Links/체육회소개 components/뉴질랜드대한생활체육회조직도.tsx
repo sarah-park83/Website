5bf992e16a09251354eed622b31b42fa3600f0aa
Sarah Park <sarahypark83@gmail.com>
2023-10-30 16:32:09 +1300

    update tree

--- client/components/Links/체육회소개 components/뉴질랜드대한생활체육회조직도.tsx
+++ client/components/Links/체육회소개 components/뉴질랜드대한생활체육회조직도.tsx
@@ -124,7 +124,7 @@ export function 뉴질랜드대한생활체육회조직도() {
                       </div>
                     </div>
                     <div className="status-id-text-container">
-                      <h6>직위 : 회장</h6>
+                      <h6>회장</h6>
                       <h6>성명 : 김웅기</h6>
                     </div>
                   </div>
@@ -140,13 +140,13 @@ export function 뉴질랜드대한생활체육회조직도() {
                         <ImageLoader
                           className="id-photo"
                           src="https://louisville.edu/enrollmentmanagement/images/person-icon/image"
-                          alt="2"
+                          alt="1"
                         />
                       </div>
                     </div>
                     <div className="status-id-text-container">
-                      <h6>직위 : 부회장</h6>
-                      <h6>성명 : 유용상</h6>
+                      <h6>고문</h6>
+                      <h6>성명 : . . .</h6>
                     </div>
                   </div>
                 </div>
@@ -161,13 +161,13 @@ export function 뉴질랜드대한생활체육회조직도() {
                         <ImageLoader
                           className="id-photo"
                           src="https://louisville.edu/enrollmentmanagement/images/person-icon/image"
-                          alt="3"
+                          alt="2"
                         />
                       </div>
                     </div>
                     <div className="status-id-text-container">
-                      <h6>직위 : 사무장</h6>
-                      <h6>성명 : 김승현</h6>
+                      <h6>부회장</h6>
+                      <h6>성명 : 유용상</h6>
                     </div>
                   </div>
                 </div>
@@ -182,18 +182,39 @@ export function 뉴질랜드대한생활체육회조직도() {
                         <ImageLoader
                           className="id-photo"
                           src="https://louisville.edu/enrollmentmanagement/images/person-icon/image"
+                          alt="3"
+                        />
+                      </div>
+                    </div>
+                    <div className="status-id-text-container">
+                      <h6>사무장</h6>
+                      <h6>성명 : 김승현</h6>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="status-position">5</div>
+              <div className="status-card">
+                <div className="status-box">
+                  <div className="status-content">
+                    <div className="status-id-photo-container">
+                      <div className="status-id-photo">
+                        <ImageLoader
+                          className="id-photo"
+                          src="https://louisville.edu/enrollmentmanagement/images/person-icon/image"
                           alt="4"
                         />
                       </div>
                     </div>
                     <div className="status-id-text-container">
-                      <h6>직위 : 감사</h6>
+                      <h6>감사</h6>
                       <h6>성명 : 안영준</h6>
                     </div>
                   </div>
                 </div>
               </div>
-              <div className="status-position">5</div>
+              <div className="status-position">6</div>
               <div className="status-card">
                 <div className="status-box">
                   <div className="status-content">
@@ -207,29 +228,8 @@ export function 뉴질랜드대한생활체육회조직도() {
                       </div>
                     </div>
                     <div className="status-id-text-container">
-                      <h6>직위 : 총무이사</h6>
+                      <h6>총무이사</h6>
                       <h6>성명 : 김도균</h6>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div className="status-position">6</div>
-              <div className="status-card">
-                <div className="status-box">
-                  <div className="status-content">
-                    <div className="status-id-photo-container">
-                      <div className="status-id-photo">
-                        <ImageLoader
-                          className="id-photo"
-                          src="https://louisville.edu/enrollmentmanagement/images/person-icon/image"
-                          alt="6"
-                        />
-                      </div>
-                    </div>
-                    <div className="status-id-text-container">
-                      <h6>직위 : 체육수석이사</h6>
-                      <h6>성명 : 이재구</h6>
                     </div>
                   </div>
                 </div>
@@ -244,13 +244,13 @@ export function 뉴질랜드대한생활체육회조직도() {
                         <ImageLoader
                           className="id-photo"
                           src="https://louisville.edu/enrollmentmanagement/images/person-icon/image"
-                          alt="7"
+                          alt="6"
                         />
                       </div>
                     </div>
                     <div className="status-id-text-container">
-                      <h6>직위 : 섭외이사</h6>
-                      <h6>성명 : 김영옥</h6>
+                      <h6>체육수석이사</h6>
+                      <h6>성명 : 이재구</h6>
                     </div>
                   </div>
                 </div>
@@ -265,13 +265,13 @@ export function 뉴질랜드대한생활체육회조직도() {
                         <ImageLoader
                           className="id-photo"
                           src="https://louisville.edu/enrollmentmanagement/images/person-icon/image"
-                          alt="8"
+                          alt="7"
                         />
                       </div>
                     </div>
                     <div className="status-id-text-container">
-                      <h6>직위 : 홍보이사</h6>
-                      <h6>성명 : 이재훈</h6>
+                      <h6>섭외이사</h6>
+                      <h6>성명 : 김영옥</h6>
                     </div>
                   </div>
                 </div>
@@ -286,12 +286,33 @@ export function 뉴질랜드대한생활체육회조직도() {
                         <ImageLoader
                           className="id-photo"
                           src="https://louisville.edu/enrollmentmanagement/images/person-icon/image"
+                          alt="8"
+                        />
+                      </div>
+                    </div>
+                    <div className="status-id-text-container">
+                      <h6>홍보이사</h6>
+                      <h6>성명 : 이재훈</h6>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="status-position">10</div>
+              <div className="status-card">
+                <div className="status-box">
+                  <div className="status-content">
+                    <div className="status-id-photo-container">
+                      <div className="status-id-photo">
+                        <ImageLoader
+                          className="id-photo"
+                          src="https://louisville.edu/enrollmentmanagement/images/person-icon/image"
                           alt="9"
                         />
                       </div>
                     </div>
                     <div className="status-id-text-container">
-                      <h6>직위 : 자문의원</h6>
+                      <h6>자문의원</h6>
                       <h6>성명 : . . .</h6>
                     </div>
                   </div>

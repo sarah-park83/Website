d0c4ca1eaa3352b1982fcb8da831ba9a6099fad2
Sarah Park <sarahypark83@gmail.com>
2023-11-15 04:32:41 +1300

    update jojikdo, excluding responsiveness

--- client/components/Links/체육회소개 components/뉴질랜드대한생활체육회조직도.tsx
+++ client/components/Links/체육회소개 components/뉴질랜드대한생활체육회조직도.tsx
@@ -1,5 +1,6 @@
 import '../../../styles/Sub-pages/subpage.css'
 import '../../../styles/Sub-pages/체육회 소개 css/뉴질랜드대한생활체육회조직도-content.css'
+
 import Dropdownmenu from '../../Dropdown/Dropdownmenu'
 import Dropdownmenu_mobile from '../../Dropdown/Dropdownmenu-mobile'
 import MobileCheck from '../../Dropdown/Mobile-check'
@@ -13,6 +14,18 @@ import logo2 from '../../../images/logo2.png'
 
 export function 뉴질랜드대한생활체육회조직도() {
   const isMobile = MobileCheck()
+
+  const statusCardStyles = {
+    marginRight: '20px',
+  }
+
+  const nameStyles = {
+    paddingLeft: '5%',
+  }
+
+  const multipleRows = {
+    marginLeft: 'auto',
+  }
 
   return (
     <div>
@@ -100,18 +113,24 @@ export function 뉴질랜드대한생활체육회조직도() {
               </h3>
             </div>
 
-            <div className="status-result-container">
+            <div className="status-result-container-jojik">
               <h5>▶ 뉴질랜드 대한생활체육회 조직도</h5>
             </div>
-            <div className="status-container">
-              <div className="logo2-container">
+            <div className="text-container">
+              <h2>뉴질랜드 대한생활체육회 조직도</h2>
+            </div>
+            <div className="status-container-jojik">
+              <div className="logo2-container" style={{ marginBottom: 0 }}>
                 <ImageLoader className="logo2" src={logo2} alt="logo" />
               </div>
-              <div className="text-container">
-                <h2>뉴질랜드 대한생활체육회 조직도</h2>
-              </div>
+
               <div className="status-position-jojik"></div>
-              <div className="status-card-jojik">
+              <div className="status-card-jojik-arrow">
+                <div className="status-id-text-container-jojik">
+                  <h6 style={{ backgroundColor: '#526D82' }}>
+                    <div className="titles-jojik">회장</div>
+                  </h6>
+                </div>
                 <div className="status-box-jojik">
                   <div className="status-content-jojik">
                     <div className="status-id-photo-container-jojik">
@@ -123,8 +142,7 @@ export function 뉴질랜드대한생활체육회조직도() {
                         />
                       </div>
                     </div>
-                    <div className="status-id-text-container-jojik">
-                      <h6>회장</h6>
+                    <div className="name">
                       <h6>김웅기</h6>
                     </div>
                   </div>
@@ -132,7 +150,12 @@ export function 뉴질랜드대한생활체육회조직도() {
               </div>
 
               <div className="status-position-jojik"></div>
-              <div className="status-card-jojik">
+              <div className="status-card-jojik-arrow">
+                <div className="status-id-text-container-jojik">
+                  <h6 style={{ backgroundColor: '#526D82' }}>
+                    <div className="titles-jojik">부회장</div>
+                  </h6>
+                </div>
                 <div className="status-box-jojik">
                   <div className="status-content-jojik">
                     <div className="status-id-photo-container-jojik">
@@ -144,8 +167,7 @@ export function 뉴질랜드대한생활체육회조직도() {
                         />
                       </div>
                     </div>
-                    <div className="status-id-text-container-jojik">
-                      <h6>부회장</h6>
+                    <div className="name">
                       <h6>유용상</h6>
                     </div>
                   </div>
@@ -153,7 +175,12 @@ export function 뉴질랜드대한생활체육회조직도() {
               </div>
 
               <div className="status-position-jojik"></div>
-              <div className="status-card-jojik">
+              <div className="status-card-jojik-arrow">
+                <div className="status-id-text-container-jojik">
+                  <h6 style={{ backgroundColor: '#526D82' }}>
+                    <div className="titles-jojik">총괄본부장</div>
+                  </h6>
+                </div>
                 <div className="status-box-jojik">
                   <div className="status-content-jojik">
                     <div className="status-id-photo-container-jojik">
@@ -165,8 +192,7 @@ export function 뉴질랜드대한생활체육회조직도() {
                         />
                       </div>
                     </div>
-                    <div className="status-id-text-container-jojik">
-                      <h6>총괄본부장</h6>
+                    <div className="name">
                       <h6>김영옥</h6>
                     </div>
                   </div>
@@ -174,7 +200,12 @@ export function 뉴질랜드대한생활체육회조직도() {
               </div>
 
               <div className="status-position-jojik"></div>
-              <div className="status-card-jojik">
+              <div className="status-card-jojik-arrow">
+                <div className="status-id-text-container-jojik">
+                  <h6 style={{ backgroundColor: '#526D82' }}>
+                    <div className="titles-jojik">사무총장</div>
+                  </h6>
+                </div>
                 <div className="status-box-jojik">
                   <div className="status-content-jojik">
                     <div className="status-id-photo-container-jojik">
@@ -186,8 +217,7 @@ export function 뉴질랜드대한생활체육회조직도() {
                         />
                       </div>
                     </div>
-                    <div className="status-id-text-container-jojik">
-                      <h6>사무총장</h6>
+                    <div className="name">
                       <h6>김승현</h6>
                     </div>
                   </div>
@@ -195,80 +225,108 @@ export function 뉴질랜드대한생활체육회조직도() {
               </div>
 
               <div className="status-position-jojik">
-                <div className="status-card-jojik">
+                <div
+                  className="status-card-jojik-arrow-upper"
+                  style={statusCardStyles}
+                >
+                  <div className="status-id-text-container-jojik">
+                    <h6 style={{ backgroundColor: '#526D82' }}>
+                      <div className="titles-jojik">총무이사</div>
+                    </h6>
+                  </div>
                   <div className="status-box-jojik">
                     <div className="status-content-jojik">
-                      <div className="status-id-photo-container-jojik1">
+                      <div className="status-id-photo-container-jojik">
                         <div className="status-id-photo-jojik">
                           <ImageLoader
-                            className="id-photo1-jojik"
+                            className="id-photo-jojik"
                             src="https://louisville.edu/enrollmentmanagement/images/person-icon/image"
                             alt="5"
                           />
                         </div>
                       </div>
-                      <div className="status-id-text-container1-jojik">
-                        <h6>총무이사</h6>
+                      <div className="name" style={nameStyles}>
                         <h6>김도균</h6>
                       </div>
                     </div>
                   </div>
                 </div>
 
-                <div className="status-card-jojik">
+                <div
+                  className="status-card-jojik-arrow-upper"
+                  style={statusCardStyles}
+                >
+                  <div className="status-id-text-container-jojik">
+                    <h6 style={{ backgroundColor: '#526D82' }}>
+                      <div className="titles-jojik">체육수석이사</div>
+                    </h6>
+                  </div>
                   <div className="status-box-jojik">
                     <div className="status-content-jojik">
-                      <div className="status-id-photo-container-jojik1">
+                      <div className="status-id-photo-container-jojik">
                         <div className="status-id-photo-jojik">
                           <ImageLoader
-                            className="id-photo1-jojik"
+                            className="id-photo-jojik"
                             src="https://louisville.edu/enrollmentmanagement/images/person-icon/image"
                             alt="6"
                           />
                         </div>
                       </div>
-                      <div className="status-id-text-container1-jojik">
-                        <h6>체육수석이사</h6>
+                      <div className="name" style={nameStyles}>
                         <h6>이재구</h6>
                       </div>
                     </div>
                   </div>
                 </div>
 
-                <div className="status-card-jojik">
+                <div
+                  className="status-card-jojik-arrow-upper"
+                  style={statusCardStyles}
+                >
+                  <div className="status-id-text-container-jojik">
+                    <h6 style={{ backgroundColor: '#526D82' }}>
+                      <div className="titles-jojik">섭외이사</div>
+                    </h6>
+                  </div>
                   <div className="status-box-jojik">
                     <div className="status-content-jojik">
-                      <div className="status-id-photo-container-jojik1">
+                      <div className="status-id-photo-container-jojik">
                         <div className="status-id-photo-jojik">
                           <ImageLoader
-                            className="id-photo1-jojik"
+                            className="id-photo-jojik"
                             src="https://louisville.edu/enrollmentmanagement/images/person-icon/image"
                             alt="7"
                           />
                         </div>
                       </div>
-                      <div className="status-id-text-container1-jojik">
-                        <h6>섭외이사</h6>
+                      <div className="name" style={nameStyles}>
                         <h6>이재행</h6>
                       </div>
                     </div>
                   </div>
                 </div>
 
-                <div className="status-card-jojik">
+                <div
+                  className="status-card-jojik-arrow-upper"
+                  style={statusCardStyles}
+                >
+                  <div className="status-id-text-container-jojik">
+                    <h6 style={{ backgroundColor: '#526D82' }}>
+                      <div className="titles-jojik">홍보이사</div>
+                    </h6>
+                  </div>
                   <div className="status-box-jojik">
                     <div className="status-content-jojik">
-                      <div className="status-id-photo-container-jojik1">
+                      <div className="status-id-photo-container-jojik">
                         <div className="status-id-photo-jojik">
                           <ImageLoader
-                            className="id-photo1-jojik"
+                            className="id-photo-jojik"
                             src="https://louisville.edu/enrollmentmanagement/images/person-icon/image"
                             alt="8"
                           />
                         </div>
                       </div>
-                      <div className="status-id-text-container1-jojik">
-                        <h6>홍보이사</h6>
+                      <div className="name" style={nameStyles}>
                         <h6>이재훈</h6>
                       </div>
                     </div>
@@ -277,7 +335,12 @@ export function 뉴질랜드대한생활체육회조직도() {
               </div>
 
               <div className="status-position-jojik"></div>
-              <div className="status-card-jojik">
+              <div className="status-card-jojik" style={multipleRows}>
+                <div className="status-id-text-container-jojik">
+                  <h6 style={{ backgroundColor: '#526D82' }}>
+                    <div className="titles-jojik">홍보이사</div>
+                  </h6>
+                </div>
                 <div className="status-box-jojik">
                   <div className="status-content-jojik">
                     <div className="status-id-photo-container-jojik">
@@ -289,8 +352,32 @@ export function 뉴질랜드대한생활체육회조직도() {
                         />
                       </div>
                     </div>
-                    <div className="status-id-text-container-jojik">
-                      <h6>감사</h6>
+                    <div className="name">
+                      <h6>서미라</h6>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="status-position-jojik"></div>
+              <div className="status-card-jojik">
+                <div className="status-id-text-container-jojik">
+                  <h6 style={{ backgroundColor: '#DDE6ED' }}>
+                    <div className="titles-jojik">감사</div>
+                  </h6>
+                </div>
+                <div className="status-box-jojik">
+                  <div className="status-content-jojik">
+                    <div className="status-id-photo-container-jojik">
+                      <div className="status-id-photo-jojik">
+                        <ImageLoader
+                          className="id-photo-jojik"
+                          src="https://louisville.edu/enrollmentmanagement/images/person-icon/image"
+                          alt="4"
+                        />
+                      </div>
+                    </div>
+                    <div className="name">
                       <h6>안영준</h6>
                     </div>
                   </div>
@@ -298,7 +385,16 @@ export function 뉴질랜드대한생활체육회조직도() {
               </div>
 
               <div className="status-position-jojik">
+                <div className="sub-heading-container-jojik">
+                  <h2 className="sub-heading-jojik">의료고문</h2>
+                </div>
+              </div>
+
+              <div className="status-position-jojik">
                 <div className="status-card-jojik">
+                  <div className="status-id-text-container-jojik">
+                    <h6>법률고문</h6>
+                  </div>
                   <div className="status-box-jojik">
                     <div className="status-content-jojik">
                       <div className="status-id-photo-container-jojik">
@@ -310,8 +406,7 @@ export function 뉴질랜드대한생활체육회조직도() {
                           />
                         </div>
                       </div>
-                      <div className="status-id-text-container-jojik">
-                        <h6>법률고문</h6>
+                      <div className="name">
                         <h6>. . .</h6>
                       </div>
                     </div>
@@ -319,6 +414,9 @@ export function 뉴질랜드대한생활체육회조직도() {
                 </div>
 
                 <div className="status-card-jojik">
+                  <div className="status-id-text-container-jojik">
+                    <h6>의료고문</h6>
+                  </div>
                   <div className="status-box-jojik">
                     <div className="status-content-jojik">
                       <div className="status-id-photo-container-jojik">
@@ -330,10 +428,8 @@ export function 뉴질랜드대한생활체육회조직도() {
                           />
                         </div>
                       </div>
-                      <div className="status-id-text-container-jojik">
-                        <h6>의료고문</h6>
-                        <h6>임경우</h6>
-                        <h6>Physiotherapist</h6>
+                      <div className="name">
+                        <h6>임경우 Physio</h6>
                       </div>
                     </div>
                   </div>
